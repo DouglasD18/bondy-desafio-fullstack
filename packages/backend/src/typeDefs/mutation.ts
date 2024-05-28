0a1052e1332cb765c275a9622e72dc3e2fce8277
Douglinhas <daguiaralcantara@gmail.com>
2024-05-28 09:16:17 -0300

    feat: craete login Mutation

--- packages/backend/src/typeDefs/mutation.ts
+++ packages/backend/src/typeDefs/mutation.ts
@@ -3,5 +3,18 @@ import gql from 'graphql-tag'
 export default gql`
   type Mutation {
     mutationTest(test: Boolean): Boolean
+    login(login: user.Login): user.User
+  }
+  
+  type User {
+    name: String!
+    email: String!
+    password: String!
+    company: String
+  }
+
+  type Login {
+    email: String!
+    password: String!
   }
 `
